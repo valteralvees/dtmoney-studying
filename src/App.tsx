8e955147bc586b13efc6556f4a5a8f9b042e42d6
Valter <valteralvees@outlook.com>
2021-07-14 08:56:32 -0300

    Componente (Header)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header";
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
   return (
-   <div className="App">
-     <h1>Hello World</h1>
-   </div>
+   <>
+     <Header/>
+     <GlobalStyles/>
+   </>
   );
 }
